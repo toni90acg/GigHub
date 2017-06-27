786260127833fc48df27befabc9a4578807e55df
Antonio Cárdenas <toni_acg@hotmail.com>
2017-06-27 20:06:49 +0200

    add button and clean controller

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,23 +1,19 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
             _context = new ApplicationDbContext();
         }
 
-        // GET: Gigs
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
