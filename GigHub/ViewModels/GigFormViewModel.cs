89804c0aa4b960546fcbbe695fa4b95cedf49c91
Antonio Cárdenas <toni_acg@hotmail.com>
2017-06-30 18:54:37 +0200

    Implementing client side validations

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,21 +1,30 @@
 ﻿using GigHub.Models;
 using System.Collections.Generic;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime {
-            get
-            {
-                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            }
-        }
+
+        public DateTime GetDateTime()
+            => DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        
     }
 }
